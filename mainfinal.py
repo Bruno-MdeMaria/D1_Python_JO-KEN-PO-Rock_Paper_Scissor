24bf577e5209af9afc7635b6aa3558148ffea037
Bruno-MdeMaria <106575354+Bruno-MdeMaria@users.noreply.github.com>
2022-07-21 00:21:20 -0300

    while + imagens

--- mainfinal.py
+++ mainfinal.py
@@ -1,60 +1,38 @@
 from importlib import import_module
+from imagens import rock
+from imagens import paper
+from imagens import scissors
+
+fim_jogo = False
+while fim_jogo == False:
+    escolha = int(input("\nO que você escolhe? Digite \n0 para Pedra, \n1 para Papel ou \n2 para Tesoura. \n= "))
+    game = [rock, paper, scissors]
+    if escolha > 3 or escolha < 0:
+        print("você escolheu um número inválido. Você perdeu!")
+    else: print(game[escolha])
+
+    import random
+    cpu = random.randint(0,2)
+    print(f"Computador escolheu: \n {game[cpu]} ")
 
 
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
-
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
-
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
-escolha = int(input("O que você escolhe? Digite 0 para Pedra, 1 para Papel ou 2 para Tesoura. \n= "))
-game = [rock, paper, scissors]
-if escolha > 3 or escolha < 0:
-    print("você escolheu um número inválido. Você perdeu!")
-else: print(game[escolha])
-
-import random
-cpu = random.randint(0,2)
-print(f"Computador escolheu: \n {game[cpu]} ")
-
-
-if escolha == 0:
-    if cpu == 1:
-        print("Você perdeu")
-    elif cpu == 2:
-        print("Você ganhou")
-    else: print("Empatou")
-elif escolha == 1:
-    if cpu == 0:
-        print("Você ganhou!")
-    elif cpu == 2:
-        print("Você perdeu!")
-    else: print("Empatou!")
-elif escolha == 2:
-    if cpu == 0:
-        print("Você perdeu!")
-    elif cpu == 1:
-        print("Você ganhou!")
-    else: print("Empatou!")
+    if escolha == 0:
+        if cpu == 1:
+            print("Você perdeu")
+        elif cpu == 2:
+            print("Você ganhou")
+        else: print("Empatou")
+    elif escolha == 1:
+        if cpu == 0:
+            print("Você ganhou!")
+        elif cpu == 2:
+            print("Você perdeu!")
+        else: print("Empatou!")
+    elif escolha == 2:
+        if cpu == 0:
+            print("Você perdeu!")
+        elif cpu == 1:
+            print("Você ganhou!")
+        else: print("Empatou!")
 
 
